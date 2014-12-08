d6b9c6737c29dd4f502f51264d45e37985bb40f5
anniemcghee <mcghee.annie@gmail.com>
2014-12-07 23:02:50 -0800

    So close on the delete button

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var db = require("./models/index.js");
+
 var app = express();
 
 app.use(bodyParser.urlencoded())
@@ -14,6 +16,11 @@ app.get('/movies/home', function (req,res) {
 })
 
 
+app.get('/movies/watchlist', function( req, res) {
+	db.Watch.findAll().done(function(err, data2){
+		res.render('movies/watchlist',{data2: data2});
+	})
+})
 
 app.get('/movies/search/', function (req, res) {
 	var searchTerm = req.query.movies;
@@ -21,15 +28,15 @@ app.get('/movies/search/', function (req, res) {
 
 	request("http://omdbapi.com/?s=" + searchTerm, function (error, response, body){
 		if (!error && response.statusCode == 200) {
-		var results = JSON.parse(body);
-		console.log(results);
-		res.render('movies/search', results)
+			var results = JSON.parse(body);
+			res.render('movies/search', results)
 		}
 		else {
-		res.render ('/movies/error')
+			res.render ('/movies/error')
 		}
 	})
 })
+
 
 app.get('/movies/:imdb', function (req, res){
 	var request = require('request');
@@ -37,31 +44,59 @@ app.get('/movies/:imdb', function (req, res){
 
 	request("http://omdbapi.com/?i=" + id +"&tomatoes=true&"+"&plot=full&", function (error, response, body){
 		if (!error && response.statusCode == 200) {
-		var results2 = JSON.parse(body);
-		res.render('movies/id', results2)
+			var results2 = JSON.parse(body);
+			res.render('movies/id', results2);
 		}
 		else {
-		res.render ('/movies/error')
+			res.render ('/movies/error');
 		}
 	})
 })
- //to star a movie as a favorite, it has to be in the :id page as a submit button and render to 
- //a link in the header - favorites. 
- //when you click, imdb code, title, year saves to database and your get a post alert
- //HIDDEN FORM FIELD IN YOUR EJS PAGE WITH EJS TAGS- change type to hidden and hdden form fields populated with info when you click 
- //watch list button. You can pass data to your post route through this hidden form
- //every starred movie gets passed to the favorite database and added to watch link
- //Also add list of movies into divs like Adrienne's and Claire's
- //Clean up backgrounds to be more dynamic
- //
 
+app.post('/movies/watchlist', function (req,res) {
+	db.Watch.findOrCreate({ where: {imdb_code: req.body.imdb_code, title: req.body.title, year: req.body.year }}).done(function(err,data,notCreated){
+			db.Watch.findAll().done(function(err, data2){
+				res.render('movies/watchlist',{data2: data2});
+		})
+	})
+})
+
+app.post('/movies/watchlist', function ( req,res ){
+	db.Watch.find({ where: { imdb_code: req.body.imdb_code  } }).then(function(row){
+  		row.destroy().success(function() {
+		res.redirect('movies/watchlist');
+  		})
+	})
+})
 
 app.get('/site/about', function (req, res) {
 	res.render('site/about');
 })
 
-app.get('/site/contact', function (req, res) {
-	res.render('site/contact');
-})
 
 app.listen(3000);
+
+
+
+// app.post('/movies/watchlist', function (req,res) {
+// 	db.Watch.findOrCreate({ where: {imdb_code: req.body.imdb_code, title: req.body.title, year: req.body.year }}).done(function(err,data,notCreated){
+// 		if(notCreated) {
+// 			db.Watch.findAll().done(function(err, data2){
+// 				res.render('movies/watchlist',{data2: data2});
+// 		})
+// 		}
+// 		else {
+// 			db.Watch.findAll().done(function(err, data2){
+// 				res.render('movies/watchlist',{data2: data2});
+// 			})
+// 		}
+// 	})
+// })
+
+// app.post('/movies/watchlist', function (req,res) {
+// 	db.Watch.create({ imdb_code: req.body.imdb_code, title: req.body.title, year: req.body.year }).done(function(err,data){
+// 		db.Watch.findAll().done(function(err, data2){
+// 			res.render('movies/watchlist',{data2: data2});
+// 		})
+// 	})
+// })
